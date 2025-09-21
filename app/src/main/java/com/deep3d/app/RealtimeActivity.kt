62f034b2613e8d79e260c50521ac4b959f840ada
Tnrxx <tanerunsal88@gmail.com>
2025-09-21 18:14:16 +0300

    Create RealtimeActivity.kt

---
+++ app/src/main/java/com/deep3d/app/RealtimeActivity.kt
@@ -0,0 +1,11 @@
+package com.deep3d.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RealtimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realtime)
+    }
+}
